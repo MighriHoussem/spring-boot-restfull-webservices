95856c6d159fd294ce6fa439a58552dfe7ec2632
Mighri <mighri.houssem.official@gmail.com>
2023-01-23 21:55:54 +0100

    Rest API User Completed

--- src/main/java/com/javaguides/springbootrestfullwebservices/controller/UserController.java
+++ src/main/java/com/javaguides/springbootrestfullwebservices/controller/UserController.java
@@ -6,6 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -24,5 +27,34 @@ public class UserController {
 
         User user1 = this.userService.createUser(user);
         return new ResponseEntity<>(user1, HttpStatus.CREATED);
+    }
+
+
+    //REST API get user by id
+    @GetMapping("{id}")
+    public ResponseEntity<User> getUserById (@PathVariable Long id) {
+        User user =  this.userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    //REST API get all users
+    @GetMapping("all")
+    public ResponseEntity<List<User>> getAllUsers () {
+        return new ResponseEntity<>(this.userService.getAllUsers(), HttpStatus.OK);
+    }
+
+
+    //REST API update user by id
+    @PutMapping("{id}/update")
+    public ResponseEntity<User> updateUserInfos(@PathVariable Long id,
+            @RequestBody User user) {
+        User updatedUser = this.userService.updateUserInfos(user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    //Rest API Delete user By Id
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteUserByID (@PathVariable("id") Long userID){
+        return new ResponseEntity<>(this.userService.deleteUserById(userID) , HttpStatus.OK);
     }
 }
--- src/main/java/com/javaguides/springbootrestfullwebservices/service/UserService.java
+++ src/main/java/com/javaguides/springbootrestfullwebservices/service/UserService.java
@@ -4,6 +4,9 @@ import com.javaguides.springbootrestfullwebservices.entity.User;
 import com.javaguides.springbootrestfullwebservices.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService {
 
@@ -17,4 +20,26 @@ public class UserService {
         return this.userRepository.save(user);
     }
 
+    public User getUserById(Long id) {
+        Optional<User> user =  this.userRepository.findById(id);
+        return user.get();
+    }
+
+    public List<User> getAllUsers () {
+        return this.userRepository.findAll();
+    }
+
+    public User updateUserInfos (User user) {
+        User user1 =  this.userRepository.findById(user.getId()).get();
+        user1.setFirstName(user.getFirstName());
+        user1.setLastName(user.getLastName());
+        user1.setEmail(user.getEmail());
+
+        return this.userRepository.save(user1);
+    }
+
+    public String deleteUserById(Long id) {
+        this.userRepository.deleteById(id);
+        return "User Successfully Deleted !";
+    }
 }
